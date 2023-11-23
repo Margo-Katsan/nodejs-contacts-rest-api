66ae415a3123ab51065dc9618ae84303c07c0c00
Margo-Katsan <123902159+Margo-Katsan@users.noreply.github.com>
2023-11-23 23:59:02 +0100

    a

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,11 +2,21 @@ const bcrypt = require("bcrypt")
 
 const jwt = require("jsonwebtoken")
 
+const gravavatar = require("gravatar")
+
+const Jimp = require("jimp");
+
+const path = require("path");
+
+const fs = require("fs/promises")
+
 const { HttpError, ctrlWrapper } = require("../helpers");
 
 const { User } = require('../models/user');
 
 const { SECRET_KEY } = process.env;
+
+const avatarsDir = path.join(__dirname, "../", "public", "avatars")
 
 const register = async (req, res) => {
   const { email, password } = req.body;
@@ -15,12 +25,16 @@ const register = async (req, res) => {
     throw HttpError(409, "Email already in use")
   }
   const hashPassword = await bcrypt.hash(password, 10);
+  const avatarURL = gravavatar.url(email)
 
-  const newUser = await User.create({ ...req.body, password: hashPassword });
+  const newUser = await User.create({ ...req.body, password: hashPassword, avatarURL });
 
   res.status(201).json({
-    email: newUser.email,
-    name: newUser.name
+    user: {
+      email: newUser.email,
+      subscription: newUser.subscription
+    }
+    
   })
 }
 
@@ -38,20 +52,27 @@ const login = async (req, res) => {
   const payload = {
     id: user._id
   }
+  const filteredUser = {
+    email: user.email,
+    subscription: user.subscription
+  }
+
   const token = jwt.sign(payload, SECRET_KEY, {expiresIn: "23h"})
-  await User.findByIdAndUpdate(user._id, {token})
+  await User.findByIdAndUpdate(user._id, { token })
+  
   res.json({
     token,
+    user: filteredUser
   })
   
 }
 
-
 const getCurrent = async (req, res) => {
-  const { email, name } = req.user;
+  const { email, subscription } = req.user;
+
   res.json({
     email,
-    name,
+    subscription,
   })
 }
 
@@ -71,12 +92,30 @@ const updateSubscription = async (req, res) => {
   res.json({
     message: "Update subscription success"
   })
-
 }
+
+const updateAvatar = async (req, res) => {
+  const { _id } = req.user;
+  const { path: tempUpload, originalname } = req.file;
+  const filename = `${_id}_${originalname}`
+  const resultUpload = path.join(avatarsDir, filename);
+
+  const image = await Jimp.read(tempUpload);
+  await image.resize(250, 250).writeAsync(resultUpload);
+
+  await fs.unlink(tempUpload);
+
+  const avatarURL = path.join("avatars", filename);
+  await User.findByIdAndUpdate(_id, { avatarURL });
+
+  res.json({avatarURL})
+}
+
 module.exports = {
   register: ctrlWrapper(register),
   login: ctrlWrapper(login),
   getCurrent: ctrlWrapper(getCurrent),
   logout: ctrlWrapper(logout),
-  updateSubscription: ctrlWrapper(updateSubscription)
+  updateSubscription: ctrlWrapper(updateSubscription),
+  updateAvatar: ctrlWrapper(updateAvatar)
 }
